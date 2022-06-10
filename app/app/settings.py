a7805813e06a30c796e0cca1d39cc4e6c4e80705
lucky-7865 <lucky.kumar@aptusdatalabs.com>
2022-06-11 01:29:02 +0530

    removed bug : settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.getenv('DB_HOST'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'NAME': os.getenv("POSTGRES_NAME"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASS"),
     }
 }
 
